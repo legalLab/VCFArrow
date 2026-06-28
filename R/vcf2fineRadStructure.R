2b5c397a7e7ad3fd39445f8c26953568ba96a27b
killidude <hrbek@evoamazon.net>
2026-06-28 00:06:09 -0500

    standardizing output reporting

--- R/vcf2fineRadStructure.R
+++ R/vcf2fineRadStructure.R
@@ -32,7 +32,9 @@ vcf2fineradstructure <- function(vcf_arrow, keep_groups = NULL,
 
   write_fineradstructure_header_cpp(setup$samples, out_file)
 
-  cli::cli_alert_info("Writing fineRADstructure: {setup$n_var} variants x {setup$n_samples} samples")
+  cli::cli_alert_info("Building fineRADstructure: {setup$n_var} variants x {setup$n_samples} samples \\
+    ({.strong {format(round(2 * setup$n_var * setup$n_samples / 1024^2), big.mark=',')}} MiB raw storage)")
+  cli::cli_alert_info("Writing fineRADstructure file...")
   cli::cli_progress_bar("Writing chunk", total = length(setup$feather_files))
 
   for (fpath in setup$feather_files) {
